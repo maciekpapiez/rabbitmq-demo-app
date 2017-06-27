3a9112e26065644f5cf714df3a9d9b4b38fcb10e
Maciej Papież <maciej.papiez@xsolve.pl>
2017-06-27 23:34:21 +0200

    Added JSON converters

--- consumer-1/src/main/java/pl/xsolve/rabbitmqdemo/ConsumerApplication.java
+++ consumer-1/src/main/java/pl/xsolve/rabbitmqdemo/ConsumerApplication.java
@@ -1,7 +1,12 @@
 package pl.xsolve.rabbitmqdemo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -30,6 +35,22 @@ public class ConsumerApplication {
         return BindingBuilder
                 .bind(queue)
                 .to(exchange);
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(final ConnectionFactory connectionFactory,
+                                                                               final Jackson2JsonMessageConverter jackson2JsonMessageConverter) {
+        SimpleRabbitListenerContainerFactory listenerContainerFactory = new SimpleRabbitListenerContainerFactory();
+
+        listenerContainerFactory.setConnectionFactory(connectionFactory);
+        listenerContainerFactory.setMessageConverter(jackson2JsonMessageConverter);
+
+        return listenerContainerFactory;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(final ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 
 }
--- producer/src/main/java/pl/xsolve/rabbitmqdemo/ProducerApplication.java
+++ producer/src/main/java/pl/xsolve/rabbitmqdemo/ProducerApplication.java
@@ -1,7 +1,11 @@
 package pl.xsolve.rabbitmqdemo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -22,4 +26,17 @@ public class ProducerApplication {
 		return new FanoutExchange(QueueDictionary.EXCHANGE_NAME);
 	}
 
+	@Bean
+	public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory,
+										 final Jackson2JsonMessageConverter jackson2JsonMessageConverter) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter);
+
+		return rabbitTemplate;
+	}
+
+	@Bean
+	public Jackson2JsonMessageConverter jackson2JsonMessageConverter(final ObjectMapper objectMapper) {
+		return new Jackson2JsonMessageConverter(objectMapper);
+	}
 }
